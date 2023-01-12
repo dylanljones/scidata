b8773e82ce7cef35e342e289f8b963e819623ad5
dylanljones <dylanljones94@gmail.com>
2023-01-12 15:26:24 +0100

    add figure root path configuration

--- scidata/root.py
+++ scidata/root.py
@@ -4,14 +4,25 @@
 #
 # Copyright (c) 2022, Dylan Jones
 
+import os
 
-_ROOT_DIR = "data"
+_DATA_DIR = "data"
+_FIGS_DIR = os.path.join(_DATA_DIR, "figs")
 
 
 def set_rootdir(path):
-    global _ROOT_DIR
-    _ROOT_DIR = path
+    global _DATA_DIR
+    _DATA_DIR = path
 
 
 def get_rootdir():
-    return _ROOT_DIR
+    return _DATA_DIR
+
+
+def set_figdir(path):
+    global _FIGS_DIR
+    _FIGS_DIR = path
+
+
+def get_figdir():
+    return _FIGS_DIR
